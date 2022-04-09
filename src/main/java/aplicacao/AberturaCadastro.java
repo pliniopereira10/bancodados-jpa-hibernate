2d089723654e95572f853d0d6711ca5304840968
pliniopereira10 <plinio.pereira10@gmail.com>
2022-04-09 17:37:32 -0300

    Adição de comentário na classe AberturaCadastro

--- src/main/java/aplicacao/AberturaCadastro.java
+++ src/main/java/aplicacao/AberturaCadastro.java
@@ -8,12 +8,14 @@ public class AberturaCadastro {
 
 	public static void main(String[] args) {
 		
+		// Aqui crio a conexão chamando as configurações do arquivo persistence.xml pelo nome bd-teste
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bd-teste");
+		//	Aqui crio objeto responsável pela manipulação da tabela
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
-		entityManager.close();
-		entityManagerFactory.close();
+		entityManager.close();	//	fim do gerenciador de manipulação da tabela
+		entityManagerFactory.close();	//	fim da conexao com o banco de dados
 
-	}
+	}	//	fim do método principal
 
-}
+}	//	fim da classe AberturaCadastro
